63037aed7abfee7ecdb6144d56b63a71fb536704
codebanas <hello@codebanas.com>
2026-09-08 03:39:25 -0400

    Wrap section barcodes with top padding

--- components/SectionBarcodeFooter.tsx
+++ components/SectionBarcodeFooter.tsx
@@ -2,7 +2,9 @@ export function SectionBarcodeFooter() {
   return (
     <div className="section-barcode-footer" aria-hidden="true">
       <span>XXXXXX</span>
-      <img src="/codes/barcode.svg" alt="" />
+      <div className="section-barcode-frame">
+        <img src="/codes/barcode.svg" alt="" />
+      </div>
     </div>
   );
 }
