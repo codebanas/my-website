c7894785dcda9e10e27c2c4f78bc1988aa97e782
codebanas <hello@codebanas.com>
2026-09-08 03:45:18 -0400

    Split section signature into equal halves

--- components/SectionBarcodeFooter.tsx
+++ components/SectionBarcodeFooter.tsx
@@ -1,7 +1,9 @@
 export function SectionBarcodeFooter() {
   return (
     <div className="section-barcode-footer" aria-hidden="true">
-      <span>XXXXXX</span>
+      <div className="section-barcode-label">
+        <span>XXXXXX</span>
+      </div>
       <div className="section-barcode-frame">
         <img src="/codes/barcode.svg" alt="" />
       </div>
